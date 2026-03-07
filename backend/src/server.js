328a827c75c96c7eda232c441e89e5ace4f7918b
barkatali937 <chmcs2026.barkatali.shaikh@gmail.com>
2026-03-07 03:15:15 -0800

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ dns.setServers(["1.1.1.1","8.8.8.8"]);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({origin: "http://localhost:5173"}))
 app.use(express.json())
